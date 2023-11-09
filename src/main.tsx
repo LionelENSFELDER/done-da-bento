68743ec60ca2a2480479a268d5e8bf1b580a24e9
Lionel Ensfelder <ensfelder.lionel@gmail.com>
2023-11-09 14:33:28 +0100

    Core: add router, mui, firebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import App from './App.tsx'
 import './index.css'
+import LoginPage from './pages/login.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Done Da Bento</h1>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/login.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import firebaseApp from '../adapters/firebase';
+import firebaseAuth from '../adapters/firebase';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import Button from '@mui/material/Button';
+
+function LoginPage() {
+  const authUser = () => {
+    console.log('authUser')
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, mail, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log('user credential', user)
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
+  console.log(firebaseApp, firebaseAuth)
+  const [mail, setMail] = useState('')
+  const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('unknow user')
+  return (
+    <>
+      <h1>{title}</h1>
+      <Box
+        component="form"
+        sx={{
+          backgroundColor: '#fff',
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          value={mail}
+          id="outlined-basic"
+          label="Mail"
+          variant="outlined"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => { setMail(event.target.value) }}
+        />
+        <TextField
+          value={password}
+          id="outlined-basic"
+          label="Mot de passe"
+          variant="outlined"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => { setPassword(event.target.value) }}
+        />
+        <Button variant="text" onClick={() => authUser()}>Text</Button>
+      </Box>
+    </>
+  )
+}
+
+export default LoginPage
